6cf672e338685d5070b4ed10142966974d216ffd
kevynthefox <carmodykevyn6@gmail.com>
2025-10-22 16:43:40 -0400

    added the background, side bar, task background, and the font.
    still need to fix the grid on the font.. and by that i mean remove the grid

---
+++ Assets/scripts/task management/task_creation.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class task_creation : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
