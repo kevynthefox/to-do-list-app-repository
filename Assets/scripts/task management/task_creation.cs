b7eec15a891dcff350fce24d8c4e55faa475283b
kevynthefox <carmodykevyn6@gmail.com>
2025-11-09 14:24:16 -0500

    added pixel code stuff, now on linux
    the ligatures still aren't working for some reason

--- Assets/scripts/task management/task_creation.cs
+++ Assets/scripts/task management/task_creation.cs
@@ -1,16 +1,22 @@
+using System.Collections.Generic;
 using UnityEngine;
+
 
 public class task_creation : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject basic_task_prefab;
+    public List<GameObject> area_tasks_go_in;
+    public int current_area;
+
+    public List<GameObject> task_list;
+
+    public void create_task()
     {
-        
+        var new_task = Instantiate(basic_task_prefab, Vector3.zero, Quaternion.identity);
+        new_task.transform.SetParent(area_tasks_go_in[current_area].transform);
+        new_task.transform.localScale = Vector3.one;
+        new_task.transform.localPosition = new Vector3(new_task.transform.localPosition.x, new_task.transform.localPosition.y, 0f);
+        task_list.Add(new_task);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
